08d2fba30804add6b3da1487f69d4c65394f0467
SamLiu <amazing_sam@outlook.com>
2020-06-14 00:54:49 +0800

    feat: 调整固定侧边栏样式

--- docs/.vuepress/components/fixedSider.vue
+++ docs/.vuepress/components/fixedSider.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrap">
-    just a plugin test
+    a hint
   </div>
 </template>
 
@@ -11,19 +11,20 @@
 </script>
 
 
-<style scoped>
-  .wrap{
-    position: fixed;
-    right: 0;
-    top: 110px;
-    width: 140px;
-    border-radius: 5px 0 0 5px;
-    padding: 20px 10px;
-    text-align: center;
-    background-color: #fff;
-    /* height: 100%; */
-    box-shadow: -1px 0 2px 0.5px #3eaf7c;
-  }
+<style lang="stylus" scoped>
+  .wrap
+    transition all 1s
+    position fixed
+    right 0
+    top 110px
+    width 50px
+    border-radius 5px 0 0 5px
+    padding 20px 10px
+    text-align center
+    background-color #fff
+    box-shadow -1px 0 4px 1px $accentColor
+    &:hover
+      width 300px
   @media screen and (max-width: 1360px) {
     .wrap{
       display: none;
