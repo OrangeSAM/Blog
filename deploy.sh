fa4215775c6b21bb15de980952ce6e833d02649c
Sam.Liu <amazing_sam@outlook.com>
2021-01-16 22:39:34 +0800

    feat: 修改配置

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:OrangeSAM/Blog.git master:gh-pages
+# git push -f git@github.com:OrangeSAM/Blog.git master:gh-pages
 
 cd -
