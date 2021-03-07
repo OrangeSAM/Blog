41362aa74ab2a9ec79eaded1bbbd9021a15ff9ca
SamLiu <amazing_sam@outlook.com>
2021-03-07 21:05:06 +0800

    feat: 调整目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -184,15 +184,6 @@ module.exports = {
         //   ]
         // },
         {
-          title: "Dart", // 必要的
-          // path: '/about/',      // 可选的, 应该是一个绝对路径
-          collapsable: true, // 可选的, 默认值是 true,
-          sidebarDepth: 2, // 可选的, 默认值是 1
-          children: [
-            "Dart/dart_primary",
-          ]
-        },
-        {
           title: "Flutter", // 必要的
           // path: '/about/',      // 可选的, 应该是一个绝对路径
           collapsable: true, // 可选的, 默认值是 true,
