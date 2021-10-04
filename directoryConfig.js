f404cec2ee4f02b4ba7c99fceb46c44ae5f1c74f
SamLiu <amazing_sam@outlook.com>
2021-10-04 19:38:09 +0800

    feat: ✅保存数据

---
+++ directoryConfig.js
@@ -0,0 +1,172 @@
+// 发现的问题
+// 目录顺序不能保证
+// 多层目录缺失 learnTeach
+// 文件丢失webpack lagou
+let config = {
+  "/CodingTool/": [
+    {
+      "title": "Webpack",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/CodingTool/Webpack/chips.md",
+        "./docs/CodingTool/Webpack/webpack_geektime.md"
+      ]
+    }
+  ],
+  "/IdeaPills/": [
+    {
+      "title": "undefined",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/IdeaPills/idea_term_1.md",
+        "./docs/IdeaPills/idea_term_10.md",
+        "./docs/IdeaPills/idea_term_11.md",
+        "./docs/IdeaPills/idea_term_12.md",
+        "./docs/IdeaPills/idea_term_2.md",
+        "./docs/IdeaPills/idea_term_3.md",
+        "./docs/IdeaPills/idea_term_4.md",
+        "./docs/IdeaPills/idea_term_5.md",
+        "./docs/IdeaPills/idea_term_6.md",
+        "./docs/IdeaPills/idea_term_7.md",
+        "./docs/IdeaPills/idea_term_8.md",
+        "./docs/IdeaPills/idea_term_9.md"
+      ]
+    }
+  ],
+  "/Interview/": [
+    {
+      "title": "undefined",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/Interview/interview.md",
+        "./docs/Interview/interview_summary.md",
+        "./docs/Interview/nowcoder_record.md",
+        "./docs/Interview/readme.md"
+      ]
+    }
+  ],
+  "/LearnTech/ ": [
+    {
+      "title": "NetCourse",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/LearnTech/NetCourse/FEer_advance.md",
+        "./docs/LearnTech/NetCourse/fe_attack.md",
+        "./docs/LearnTech/NetCourse/js_core.md",
+        "./docs/LearnTech/NetCourse/js_deep.md",
+        "./docs/LearnTech/NetCourse/geektime_git.md",
+        "./docs/LearnTech/NetCourse/office_ability.md"
+      ]
+    }
+  ],
+  "/LeetCode/": [
+    {
+      "title": "undefined",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/LeetCode/1.two_num_sum.md",
+        "./docs/LeetCode/121.chance_to_trade.md",
+        "./docs/LeetCode/13.romeToInteger.md",
+        "./docs/LeetCode/14.comon_prefix.md",
+        "./docs/LeetCode/189.reverse_array.md",
+        "./docs/LeetCode/20.valid_brace.md",
+        "./docs/LeetCode/26.delete_duplicate_in_array.md",
+        "./docs/LeetCode/27.delete_item.md",
+        "./docs/LeetCode/28.achieve_strstr.md",
+        "./docs/LeetCode/3.lengthOfLongestSubstring.md",
+        "./docs/LeetCode/35.find_insert_position.md",
+        "./docs/LeetCode/4.findMedianSortedArrays.md",
+        "./docs/LeetCode/67.binary_sum.md",
+        "./docs/LeetCode/7.integer_reverse.md",
+        "./docs/LeetCode/9.palindromic.md"
+      ]
+    }
+  ],
+  "/Others/": [
+    {
+      "title": "undefined",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/Others/anything_about_wechat.md",
+        "./docs/Others/bug_analysis.md",
+        "./docs/Others/cheerio-readme.md",
+        "./docs/Others/comlete_determine_sytem.md",
+        "./docs/Others/command_line_trick.md",
+        "./docs/Others/common_ag.md",
+        "./docs/Others/cycling_taiwan.md",
+        "./docs/Others/FEwechater.md",
+        "./docs/Others/frame_lib_sdk.md",
+        "./docs/Others/get_my_money_back.md",
+        "./docs/Others/get_outdoor_info.md",
+        "./docs/Others/life_efficiency.md",
+        "./docs/Others/make_blog.md",
+        "./docs/Others/MVC、MVP&MVVM.md",
+        "./docs/Others/recommend_an_app.md",
+        "./docs/Others/repair_of_nuc.md",
+        "./docs/Others/the_cognition_gain_in_market.md",
+        "./docs/Others/the_thanks_to_zcl.md",
+        "./docs/Others/time_tracker.md",
+        "./docs/Others/try_to_touch_the_market.md",
+        "./docs/Others/wechatread_use.md",
+        "./docs/Others/wrong.md"
+      ]
+    }
+  ],
+  "/Projects/": [
+    {
+      "title": "undefined",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/Projects/make_blog_with_vuepress.md",
+        "./docs/Projects/map_of_data_center.md",
+        "./docs/Projects/music_bulk_download.md",
+        "./docs/Projects/node_getArticle.md",
+        "./docs/Projects/node_todo.md",
+        "./docs/Projects/page_screenshot.md",
+        "./docs/Projects/readme.md",
+        "./docs/Projects/puppeteer_get_image.md",
+        "./docs/Projects/vue_wheels_button.md",
+        "./docs/Projects/vue_wheels_compos_name.md",
+        "./docs/Projects/vue_wheels_grid.md",
+        "./docs/Projects/vue_wheels_layout.md",
+        "./docs/Projects/vue_wheels_popover.md",
+        "./docs/Projects/vue_wheels_input.md",
+        "./docs/Projects/vue_wheels_start.md",
+        "./docs/Projects/vue_wheels_primary_conclusion.md",
+        "./docs/Projects/vue_wheels_tabs.md",
+        "./docs/Projects/vue_wheels_toast.md"
+      ]
+    }
+  ],
+  "/Reading/": [
+    {
+      "title": "undefined",
+      "collapsable": true,
+      "sidebarDepth": 4,
+      "children": [
+        "./docs/Reading/Benjamin_Franklin.md",
+        "./docs/Reading/charles_munger.md",
+        "./docs/Reading/civilization_modernization_valueInvestment_china.md",
+        "./docs/Reading/dog_money.md",
+        "./docs/Reading/how_to_do_well_in_internet.md",
+        "./docs/Reading/how_to_learn_efficiently.md",
+        "./docs/Reading/index_funds.md",
+        "./docs/Reading/invest_for_decades.md",
+        "./docs/Reading/readme.md",
+        "./docs/Reading/replay.md",
+        "./docs/Reading/the_rich_father_investment.md",
+        "./docs/Reading/value_what_i_thought_during_the_investment.md",
+        "./docs/Reading/writing_is_investing.md",
+        "./docs/Reading/xiaomin_qin.md",
+        "./docs/Reading/xiaoping_cushijian.md"
+      ]
+    }
+  ]
+}
--- docs/LearnTech/Javascript/js_proto_chain.md
+++ docs/LearnTech/Javascript/js_proto_chain.md
@@ -135,3 +135,12 @@ a.role()
 Admin.prototype.__proto__ = Object.create(User.prototype)
 
 ```
+---
+
+2021.9.26补充
+- 所有的引用类型（数组、对象、函数），都具有对象特性，即可自由扩展属性（除了"null"）以外
+- 所有的引用类型（数组、对象、函数），都有一个__proto__ （隐式原型）属性，属性值是一个普通的对象
+- 所有的函数，都有一个 prototype （显式原型）属性，属性值也是一个普通的对象
+- 所有的引用类型（数组、对象、函数），__proto__ 属性值指向它的构造函数的 prototype 属性值
+- 当试图得到一个对象（引用类型）的某个属性时，如果这个对象本身没有这个属性，那么会去它的 __proto__（即它的构造函数的 prototype）中寻找
+来自https://mp.weixin.qq.com/s/tBzP7id6FNlp0SvHqHI_5Q
