ff73ece9538b79154f31a2163d1ad932a0aeadc4
SamLiu <amazing_sam@outlook.com>
2022-02-27 21:17:08 +0800

    feat: ✅增加二维码

--- docs/.vuepress/components/fixedSider.vue
+++ docs/.vuepress/components/fixedSider.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="wrap">
-    a hint
+    <div class="tips">扫码添加我的微信</div>
+    <div class="img-wrap">
+      <img src="../public/personal.jpg" alt="个人">
+      <div>个人号</div>
+      <img src="../public/official.jpg" alt="公众号">
+      <div>公众号</div>
+    </div>
   </div>
 </template>
 
@@ -13,21 +19,36 @@
 
 <style lang="stylus" scoped>
   .wrap
-    transition all 1s
+    animation-name breath
+    animation-duration 3000ms
+    animation-iteration-count infinite
     position fixed
     right 0
     top 110px
-    width 50px
     border-radius 5px 0 0 5px
     padding 20px 10px
     text-align center
     background-color #fff
     box-shadow -1px 0 4px 1px $accentColor
-    &:hover
-      width 300px
-  @media screen and (max-width: 1360px) {
-    .wrap{
-      display: none;
+    .tips
+      font-size 20px
+      margin-bottom 20px
+    .img-wrap
+      display flex
+      flex-direction column
+      align-items center
+      img
+        width 150px
+
+  @keyframes breath {
+    0% {
+      transform scale(.95);
+    }
+    50% {
+      transform scale(1)
+    }
+    100% {
+      transform scale(.95);
     }
   }
 </style>
---
+++ docs/LearnTech/Javascript/window上有用的属性和方法.md
@@ -0,0 +1,7 @@
+---
+title: window上有用的属性和方法
+---
+window.client
+少见但有用的属性和方法
+window.chrome
+window.performance
