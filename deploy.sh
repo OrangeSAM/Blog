d14d12598b7c0855858a1d771b82f15d80b84367
SamLiu <amazing_sam@outlook.com>
2023-03-13 16:00:12 +0800

    fix: 🚫修复部署命令的分支名变动

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:OrangeSAM/Blog.git master:gh-pages
+git push -f git@github.com:OrangeSAM/Blog.git main:gh-pages
 
 cd -
